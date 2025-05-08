6101476ee16a75b305c0e36053e73e45a3e2383c
yousrahanif <yousrahanif26@gmail.com>
2025-05-07 20:20:10 -0400

    Add solutions and blog content

--- README.md
+++ README.md
@@ -8,7 +8,10 @@ unknown: It can be any type for now, but TypeScript will make you check the type
 
 never: never means a value will not occur in a normal situation. This often happens with functions that always throw an error.
 
+
+
 6. How does TypeScript help in improving code quality and project maintainability?
 
 Answer: As the name suggests, TypeScript tells about the type, making it easier for developers to know what kind of data a variable is expecting. In contrast, with JavaScript, just looking at the variable names doesn't tell us what type of value is being stored. TypeScript also checks for errors at compile time, so we can catch errors before running the code, which helps save time on debugging. Additionally, when projects get bigger, TypeScript keeps things organized by tracking changes.
+
 
---
+++ src/assignment1.ts
@@ -0,0 +1,118 @@
+function formatString(input:string, toUpper?: boolean): string{
+    if(toUpper || toUpper===undefined){
+        return input.toUpperCase();
+    }
+    else{
+       return input.toLowerCase();
+    }
+}
+
+
+
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+   let newItems = items.filter((item)=>item.rating>=4.0)
+    return newItems
+
+    
+   } 
+   
+   function concatenateArrays<T>(...arrays: T[][]): T[]{
+  let newArray: T[]=[]
+  for (const arr of arrays){
+    for(const val of arr){
+        newArray.push(val)
+    }
+
+
+  }
+  return newArray
+   }
+
+class Vehicle {
+    private make: string;
+    private year: number;
+    constructor(make:string, year:number){
+        this.make=make;
+        this.year=year;
+    }
+     getInfo(): string{
+        return `Make: ${this.make}, Year: ${this.year}`
+    }
+
+}
+class Car extends Vehicle{
+    private model:string;
+    constructor(make:string, year:number, model:string){
+        super(make,year)
+        this.model=model
+        
+    }
+    getModel(): string{
+       return `Model: ${this.model}`
+    }
+    
+}
+function processValue(value: string | number): number{
+    if(typeof(value)==="string"){
+        return value.length;
+
+    }
+  
+        return value*2;
+
+ 
+}
+
+interface Product {
+    name: string;
+    price: number;
+  }
+  
+  function getMostExpensiveProduct(products: Product[]): Product | null{
+   if (products.length===0) {
+    return null
+   }
+    let highest=products[0]
+    for (const prod of products){
+        if(prod.price>highest.price){
+            highest=prod
+        }
+    }
+    return highest
+  }
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  function getDayType(day: Day): string{
+    if(day===Day.Saturday || day===Day.Sunday){
+        return "Weekend"
+    }
+    return "Weekday"
+  }
+
+
+  async function squareAsync(n: number): Promise<number>{
+    return new Promise<number>((resolve, reject)=>{
+        if(n<0){
+           return reject("Error: Negative number not allowed")
+          
+        }
+
+
+        else{
+            setTimeout(()=>{
+
+resolve(n*n)
+            },1000)
+         
+       }
+        
+    })
+  }
